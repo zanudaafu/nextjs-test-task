ef6fefa6c8770c9da00adcf8324571101ada38ce
Zanuda <zanudaafu@gmail.com>
2025-04-03 23:35:17 +0300

    add dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { ThemeProvider } from "@/components/theme-provider"
 
 export const metadata = {
     title: 'Test',
@@ -7,8 +8,18 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" suppressHydrationWarning>
+            <head />
+            <body>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     )
 }
