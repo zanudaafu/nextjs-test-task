9019255154375a6110eebc07e084eff15a707fe7
Zanuda <zanudaafu@gmail.com>
2025-04-08 08:04:28 +0300

    cleanup previous test pages

--- src/app/api/hello/route.ts
+++
@@ -1,6 +0,0 @@
-'use server'
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({ message: "Hello world!" });
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,6 @@
 'use client'  // required for client-side fetch
-import { useEffect, useState } from 'react'
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-    const [message, setMessage] = useState<string | null>(null)
-
-    useEffect(() => {
-        fetch('/api/hello')
-            .then(res => res.json())
-            .then(data => setMessage(data.message))
-            .catch(err => setMessage('Failed to load'))
-    }, [])
-
-    return (
-        <main className="flex min-h-screen items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white text-4xl font-bold">
-            <div>Tailwind CSS is working 🎉</div>
-            {message ? message : 'Loading...'}
-        </main>
-    )
+    redirect('/users');
 }
